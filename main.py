4dc40311ef0dcd814f30c74441c3f6652cf6a830
ADmitryR <minecraftdag@yandex.ru>
2025-11-02 21:13:22 +0300

    0.0.1

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def home()->dict[str,str]:
+	return {"message": "Hello World"}
+
+
+@app.get("/contacts")
+async def contacts() ->int:
+	return 34
+
+posts = [{"id":1,"title":"news 1","body":"Text 1"},
+		 {"id":2,"title":"news 2","body":"Text 2"},
+		 {"id":3,"title":"news 3","body":"Text 3"},]
+
+
+@app.get("/items")
+async def items() ->list:
+	return posts
+
+@app.get("/items/{id}")
+async def items(id :int) ->dict:
+	for post in posts:
+		if post["id"] == id:
+			return post
