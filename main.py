0f1411136c933ac3b21dd3ddc31dc1320387d72c
ADmitryR <minecraftdag@yandex.ru>
2025-11-02 22:18:21 +0300

    0.1.1

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 from fastapi import FastAPI ,HTTPException
-from typing import Optional
+from typing import Optional, List, Dict
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-async def home()->dict[str,str]:
-	return {"message": "Hello World"}
-
-
-@app.get("/contacts")
-async def contacts() ->int:
-	return 34
+class Post(BaseModel):
+	id:int
+	title:str
+	body:str
 
 posts = [{"id":1,"title":"news 1","body":"Text 1"},
 		 {"id":2,"title":"news 2","body":"Text 2"},
 		 {"id":3,"title":"news 3","body":"Text 3"},]
 
 
+
+
 @app.get("/items")
-async def items() ->list:
-	return posts
+async def items() ->list[Post]:
+	return [Post(**post) for post in posts]
 
 @app.get("/items/{id}")
-async def items(id :int) ->dict:
+async def items(id :int) ->Post:
 	for post in posts:
 		if post["id"] == id:
-			return post
+			return Post(**post)
 		
 	raise HTTPException(status_code=404,detail="Post not found")
 
 
 @app.get("/search/")
-async def search(post_id: Optional[int]=None)->dict:
+async def search(post_id: Optional[int]=None)->Dict[str,Optional[Post]]:
 	if post_id:
 		for post in  posts:
 			if post["id"] == post_id:
-				return post
+				return {"data":Post(**post)}
 		raise HTTPException(status_code=404,detail="post not found")
 	else:
-		return {"data": "No post id provided"}
+		return {"data": None}
